f3a03a25bfb7a07bd3ea9262e5204f8bd2b0d550
Alex2021cyberhuman <alex2021cyberhuman@gmail.com>
2022-01-03 17:27:49 +0500

    feat: Add consumer classes

---
+++ Conduit.Articles.BusinessLogicLayer/RegisterUserEventConsumer.cs
@@ -0,0 +1,13 @@
+﻿using Conduit.Shared.Events.Models.Users.Register;
+using Conduit.Shared.Events.Services;
+
+namespace Conduit.Articles.BusinessLogicLayer;
+
+public class RegisterUserEventConsumer : IEventConsumer<RegisterUserEventModel>
+{
+    public Task ConsumeAsync(
+        RegisterUserEventModel message)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Conduit.Articles.BusinessLogicLayer/UpdateUserEventConsumer.cs
@@ -0,0 +1,12 @@
+﻿using Conduit.Shared.Events.Services;
+
+namespace Conduit.Articles.BusinessLogicLayer;
+
+public class UpdateUserEventConsumer : IEventConsumer<UpdateUserEventConsumer>
+{
+    public Task ConsumeAsync(
+        UpdateUserEventConsumer message)
+    {
+        throw new NotImplementedException();
+    }
+}
