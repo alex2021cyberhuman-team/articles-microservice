3996fdee8e64c3543919fa301bc4e83d8ce1b532
Alex2021cyberhuman <alex2021cyberhuman@gmail.com>
2022-01-03 17:38:25 +0500

    feat: Add followings consumers

---
+++ Conduit.Articles.BusinessLogicLayer/CreateFollowingEventConsumer.cs
@@ -0,0 +1,13 @@
+﻿using Conduit.Shared.Events.Models.Profiles.CreateFollowing;
+using Conduit.Shared.Events.Services;
+
+namespace Conduit.Articles.BusinessLogicLayer;
+
+public class CreateFollowingEventConsumer : IEventConsumer<CreateFollowingEventModel>
+{
+    public Task ConsumeAsync(
+        CreateFollowingEventModel message)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Conduit.Articles.BusinessLogicLayer/RemoveFollowingEventConsumer.cs
@@ -0,0 +1,13 @@
+﻿using Conduit.Shared.Events.Models.Profiles.RemoveFollowing;
+using Conduit.Shared.Events.Services;
+
+namespace Conduit.Articles.BusinessLogicLayer;
+
+public class RemoveFollowingEventConsumer : IEventConsumer<RemoveFollowingEventModel>
+{
+    public Task ConsumeAsync(
+        RemoveFollowingEventModel message)
+    {
+        throw new NotImplementedException();
+    }
+}
