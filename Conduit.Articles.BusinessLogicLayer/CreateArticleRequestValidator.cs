879fdaeb171b00e7e97109d2442bae0d0746c8ac
Alex2021cyberhuman <alex2021cyberhuman@gmail.com>
2022-01-03 18:07:44 +0500

    feat: Add validator implementation

---
+++ Conduit.Articles.BusinessLogicLayer/CreateArticleRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Conduit.Articles.DomainLayer;
+
+namespace Conduit.Articles.BusinessLogicLayer;
+
+public class
+    CreateArticleRequestValidator : IValidator<CreateArticle.Request>
+{
+    public Task<ICollection<ValidationResult>> ValidateAsync(
+        CreateArticle.Request entityToValidate,
+        CancellationToken cancellationToken = default)
+    {
+        var results = new List<ValidationResult>();
+        entityToValidate.ValidateObject(results);
+        return Task.FromResult<ICollection<ValidationResult>>(results);
+    }
+}
